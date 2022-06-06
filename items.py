194336c408780a2b9a8a391c156372aeeec1cbae
Irene Arabiourrutia Galdos <32264889+irenearabiourrutia@users.noreply.github.com>
2022-06-06 14:18:05 +0200

    Add files via upload

---
+++ items.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class DataspiderItem(scrapy.Item):
+    Reviews(scrapy.Item):
+    Comentario=scrapy.Field()
+    Estrellas=scrapy.Field()
+    Fecha=scrapy.Field()
+    Banco=scrapy.Field()
+    Producto=scrapy.Field()
+    Tipo=scrapy.Field()
+    Votos=scrapy.Field()
+    Puntuacion=scrapy.Field()
+    PorRecomendacion=scrapy.Field()
+    Votos2=scrapy.Field()
+    Usuario=scrapy.Field()
+
+
+class Questions(scrapy.Item):
+    TituloPregunta=scrapy.Field()
+    NumeroRespuestas=scrapy.Field()
+    TextoPregunta=scrapy.Field()
+    NumeroVisitas=scrapy.Field()
+    Fecha=scrapy.Field()
+    Tema=scrapy.Field()
